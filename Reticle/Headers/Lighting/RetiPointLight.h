2290ba7d2703b0621e5f198447cbf7abd5519666
Diganta Mukhopadhyay <diganta.ghs@gmail.com>
2017-07-24 18:22:23 +0530

    Added class

---
+++ Reticle/Headers/Lighting/RetiPointLight.h
@@ -0,0 +1,11 @@
+#ifndef RETIPOINTLIGHT_H_INCLUDED
+#define RETIPOINTLIGHT_H_INCLUDED
+
+#include <Math/RetiTransform.h>
+
+class RetiPointLight
+{
+    RetiTransform transf;
+};
+
+#endif // RETIPOINTLIGHT_H_INCLUDED
